3758906b6ca44d5aad96e30c75c98000a88fc038
Felipe Leitão <fesilva@outlook.com.br>
2016-07-03 16:02:51 -0300

    Trabalhando com data Annotations

--- NetCoders.SisCliente.UI.WEB/Models/ClienteMOD.cs
+++ NetCoders.SisCliente.UI.WEB/Models/ClienteMOD.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,8 +10,16 @@ namespace NetCoders.SisCliente.UI.WEB.Models
     {
         public int Codigo { get; set; }
 
+        //O Data Anottations Required, Serve pra dizer que a minha propriedade
+        //è obrigatória.
+        [Required(ErrorMessage = "Ow digita o nome ae campeão")]
         public string Nome { get; set; }
 
+        [Required]
+        //O Data Annotations EmailAddress serve para dizer que a minha
+        //Propriedade é um Email, ou seja, ele vai fazer uma verficação
+        //simples, se o campo é um email
+        [EmailAddress]
         public string Email { get; set; }
     }
 }
