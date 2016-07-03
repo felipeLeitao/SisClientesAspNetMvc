88095482af8edd36a80c7d480cde9ebec6af921b
Felipe Leitão <fesilva@outlook.com.br>
2016-07-03 14:44:50 -0300

    first commit

---
+++ NetCoders.SisCliente.UI.WEB/Models/ClienteMOD.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NetCoders.SisCliente.UI.WEB.Models
+{
+    public class ClienteMOD
+    {
+    }
+}
